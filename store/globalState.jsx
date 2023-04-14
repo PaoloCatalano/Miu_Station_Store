205d76d09ae4b8740f9d75e2e2111b3c1bbc3aed
Paolo Catalano <paolo12587@gmail.com>
2023-04-14 19:19:57 +0200

    no console.log

--- store/globalState.jsx
+++ store/globalState.jsx
@@ -213,12 +213,12 @@ const AppProvider = ({ children }) => {
   useEffect(() => {
     Router.events.on("routeChangeStart", (url) => {
       isLoading(true);
-      console.log("start changing to ", url);
+      // console.log("start changing to ", url);
     });
 
     Router.events.on("routeChangeComplete", () => {
       isLoading(false);
-      console.log("route changing finished");
+      // console.log("route changing finished");
     });
 
     Router.events.on("routeChangeError", () => {
